948a4e4126072986a3f4c38d71f5223b50ce38b0
Jorge Landaverde <jorge100394@gmail.com>
2019-05-18 18:27:23 -0700

    Updates

---
+++ precache-manifest.50bc4c94707ccf18282d22acb1edef12.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "734979005ce61f91832a4a1d0c817f3c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "841a3f607d0bb9144790",
+    "url": "/static/css/main.84c8d362.chunk.css"
+  },
+  {
+    "revision": "d2cd71af08ef8540f3dc",
+    "url": "/static/js/2.cc0c5c28.chunk.js"
+  },
+  {
+    "revision": "841a3f607d0bb9144790",
+    "url": "/static/js/main.c00e8bd8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8f98783ae9b881535bd892657584c180",
+    "url": "/static/media/NasaCollage.8f98783a.jpg"
+  },
+  {
+    "revision": "01e3ecb8836f06ccde5322c12251b012",
+    "url": "/static/media/QualcommHackJuntos.01e3ecb8.jpg"
+  },
+  {
+    "revision": "b939e9bb24b4d3f9b028c6dc2f929bae",
+    "url": "/static/media/cpp.b939e9bb.svg"
+  },
+  {
+    "revision": "56da850042e8c91a774fc771d5a76535",
+    "url": "/static/media/css.56da8500.svg"
+  },
+  {
+    "revision": "bc213bb627d7a2d237d6eeff568ce9f7",
+    "url": "/static/media/fbYoPuedoProg.bc213bb6.jpg"
+  },
+  {
+    "revision": "76d344f08412543c74ccd1fb2493d607",
+    "url": "/static/media/github_logo.76d344f0.svg"
+  },
+  {
+    "revision": "6fabdf235ac27a92cc101a65416ca534",
+    "url": "/static/media/html5.6fabdf23.svg"
+  },
+  {
+    "revision": "d419db0eeace3783c62476bd1674c57f",
+    "url": "/static/media/java1.d419db0e.svg"
+  },
+  {
+    "revision": "31bdbaa1fab25a9d8e3e287a0301279e",
+    "url": "/static/media/jorge_antonio_landaverde_resume.31bdbaa1.pdf"
+  },
+  {
+    "revision": "d4530eb392e65ae0bd591303e03bdf90",
+    "url": "/static/media/js.d4530eb3.svg"
+  },
+  {
+    "revision": "00a4ed1c04fafb672e1044389044147b",
+    "url": "/static/media/profpicsquare.00a4ed1c.jpg"
+  },
+  {
+    "revision": "9587af90b3ebaf3bb3cf114b7afd7c9d",
+    "url": "/static/media/python.9587af90.svg"
+  },
+  {
+    "revision": "5de7e7d4877f48687a43aa731a32d01c",
+    "url": "/static/media/reactB.5de7e7d4.svg"
+  }
+]);
